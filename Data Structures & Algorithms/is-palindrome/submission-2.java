5f7ff745963a7cb3e4296aded312edde76ddf98d
Dhruv Mehta <74912977+dhroof@users.noreply.github.com>
2026-09-03 00:31:08 +0530

    Add: is-palindrome - submission-2

---
+++ Data Structures & Algorithms/is-palindrome/submission-2.java
@@ -0,0 +1,33 @@
+class Solution {
+    public boolean isPalindrome(String s) {
+        int i = 0;
+        int j = s.length() - 1;
+        char c1;
+        char c2;
+        while (i <= j) {
+            c1 = Character.toLowerCase(s.charAt(i));
+            c2 = Character.toLowerCase(s.charAt(j));
+
+            // Increment left pointer if c1 is not alpha-numeric
+            if (!Character.isLetterOrDigit(c1)) {
+                i++;
+                continue;
+            }
+
+            // Decrement right pointer if c2 is not alpha-numeric
+            if (!Character.isLetterOrDigit(c2)) {
+                j--;
+                continue;
+            }
+
+            if (c1 != c2) {
+                return false;
+            }
+            
+            i++;
+            j--;
+        }
+
+        return true;
+    }
+}
