9afb254f9c94377586f7c9971a90dec34b655984
Dhruv Mehta <74912977+dhroof@users.noreply.github.com>
2026-09-05 01:51:21 +0530

    Add: buy-and-sell-crypto - submission-6

---
+++ Data Structures & Algorithms/buy-and-sell-crypto/submission-6.java
@@ -0,0 +1,19 @@
+class Solution {
+    public int maxProfit(int[] prices) {
+        int ans = 0;
+
+        int ptr1 = 0;
+        int ptr2 = 1;
+        while (ptr2 < prices.length) {
+            while (ptr1 < ptr2 && ptr2 < prices.length && prices[ptr2] >= prices[ptr1]) {
+                ans = Math.max(ans, prices[ptr2] - prices[ptr1]);
+                ptr2++;
+            }
+
+            ptr1 = ptr2;
+            ptr2++;
+        }
+
+        return ans;
+    }
+}
