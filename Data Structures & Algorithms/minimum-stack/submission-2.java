8c1e3d576c72084deeabaa3a7fe6c2a9a15a4381
Dhruv Mehta <74912977+dhroof@users.noreply.github.com>
2026-09-05 16:08:08 +0530

    Add: minimum-stack - submission-2

---
+++ Data Structures & Algorithms/minimum-stack/submission-2.java
@@ -0,0 +1,32 @@
+class MinStack {
+    private ArrayList<Integer> stack;
+    private ArrayList<Integer> minArr;
+
+    public MinStack() {
+        stack = new ArrayList<>();
+        minArr = new ArrayList<>();
+    }
+
+    public void push(int val) {
+        stack.add(val);
+        if (minArr.size() == 0 || minArr.get(minArr.size() - 1) >= val) {
+            minArr.add(val);
+        }
+    }
+
+    public void pop() {
+        int temp = stack.get(stack.size() - 1);
+        stack.remove(stack.size() - 1);
+        if (minArr.get(minArr.size() - 1).equals(temp)) {
+            minArr.remove(minArr.size() - 1);
+        }
+    }
+
+    public int top() {
+        return stack.get(stack.size() - 1);
+    }
+
+    public int getMin() {
+        return minArr.get(minArr.size() - 1);
+    }
+}
